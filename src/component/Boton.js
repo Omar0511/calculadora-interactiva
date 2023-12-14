6e48e76809ece544954cbf49b51cb38aa062c0d3
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-12-13 22:21:04 -0600

    Añadiendo CSS a los botones

--- src/component/Boton.js
+++ src/component/Boton.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../css/styles.css';
 
 function Boton(props)
 {
+    const operador = valor => {
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
+    };
+
     return (
         <div
-            className={` boton-contenedor ${ operador( props.children ) ? 'operador' : null } `}
+            className={` boton-contenedor ${ operador( props.children ) ? 'operador' : '' }`.trimEnd() }
         >
             {props.children}
         </div>
